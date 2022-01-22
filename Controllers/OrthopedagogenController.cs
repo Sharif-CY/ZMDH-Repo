9095994086f3bee55b7f5726319a0b01906da2b0
sharif.choudry <sharif.choudry@gmail.com>
2022-01-22 12:07:38 +0100

    orthopedagogen 9/10 done hulpverleners added on DB zoek filter done

--- Controllers/OrthopedagogenController.cs
+++ Controllers/OrthopedagogenController.cs
@@ -12,9 +12,37 @@ namespace WDPR.Controllers
     public class OrthopedagogenController : Controller
     {
 
+        private readonly MyContext _context;
+
+        public OrthopedagogenController(MyContext context)
+        {
+            _context = context;
+        }
+
+
+        [Route("/Orthopedagogen")]
+        [HttpGet]
         public IActionResult Index()
         {
+            ViewBag.Orthopedagogen = _context.Hulpverlener;
             return View();
         }
+
+        [Route("/Orthopedagogen")]
+        [HttpPost]
+        public IActionResult Index(string VoorNaam)
+        {
+            ViewBag.zoekterm = VoorNaam;
+            if(VoorNaam != null){
+                ViewBag.Orthopedagogen = _context.Hulpverlener.Where(x => x.VoorNaam.Contains(VoorNaam) || x.achterNaam.Contains(VoorNaam));
+            }
+            else{
+                ViewBag.Orthopedagogen = _context.Hulpverlener;
+            }
+            
+            return View("Index");
+        }
+
+        
     }
 }
